cbae406d5866dc5558d1942adc9c09d4256fc014
Mani Khanuja <mani.khanuja@pb.com>
2015-05-24 01:40:41 +0530

    Update run_analysis.R
    Script is written for Windows Platform.

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
-##################### Download & unzip accelerometers Data ##################################
+##################### Download & unzip accelerometers Data #########################
 ####################################################################################
 url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 destinationFolder <- getwd()
